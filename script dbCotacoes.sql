04210327edd6392974fe4f8cf962f94aad43eb06
davaslindo <149907772+davaslindo@users.noreply.github.com>
2024-09-27 00:00:24 -0300

    Update script dbCotacoes.sql

--- script dbCotacoes.sql
+++ script dbCotacoes.sql
@@ -8,14 +8,16 @@ CREATE TABLE cotacao_dolar (
 CREATE OR REPLACE PROCEDURE inserir_cotacao_dolar(
     data_param DATE,
     hora_param TIME,
-    cotacao_param DECIMAL
-) AS
+    cotacao_param DECIMAL(10, 2)
+) 
+LANGUAGE plpgsql
+AS $$
 BEGIN
     INSERT INTO cotacao_dolar (data, hora, cotacao)
     VALUES (data_param, hora_param, cotacao_param);
-    
-    COMMIT;
-    
+
+    -- A transação é controlada automaticamente, então não precisa de COMMIT aqui.
     RAISE NOTICE 'Dados inseridos com sucesso: Data=%, Hora=%, Cotação=%', data_param, hora_param, cotacao_param;
 END;
-$$ LANGUAGE plpgsql;
+$$;
+
